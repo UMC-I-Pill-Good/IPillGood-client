6124b1b071bb487176e1f5ff4566741fdd91d3f1
정찬원 <jcw0522@gachon.ac.kr>
2026-07-09 03:33:36 +0900

    [#6] feat: IconButton 공통 컴포넌트 구현

--- ipillgood-client/src/app/(public)/example/page.tsx
+++ ipillgood-client/src/app/(public)/example/page.tsx
@@ -1,5 +1,13 @@
+import IconButton from '@/shared/components/button/IconButton';
+import { ChevronLeft, X } from 'lucide-react';
+
 const ExamplePage = () => {
-  return <div></div>;
+  return (
+    <div className='p-4 flex flex-col gap-4'>
+      <IconButton icon={<ChevronLeft size={26} />} />
+      <IconButton icon={<X size={22} />} disabled={true} />
+    </div>
+  );
 };
 
 export default ExamplePage;
--- ipillgood-client/src/shared/components/button/IconButton.tsx
+++ ipillgood-client/src/shared/components/button/IconButton.tsx
@@ -1,5 +1,29 @@
-const IconButton = () => {
-  return <div></div>;
+import { cn } from '@/shared/utils/cn';
+
+interface IconButtonProps {
+  onClick?: () => void;
+  className?: string;
+  icon: React.ReactNode;
+  disabled?: boolean;
+}
+
+const IconButton = ({ onClick, className, icon, disabled = false }: IconButtonProps) => {
+  return (
+    <button
+      type='button'
+      onClick={onClick}
+      disabled={disabled}
+      className={cn(
+        'h-9 w-9 shrink-0 rounded-full flex items-center justify-center text-neutral-800 shadow-[0_4px_4px_rgba(126,131,135,0.1)] backdrop-blur-xl transition-all',
+        'bg-white/60 hover:bg-white active:bg-primary-300/30',
+        'ring ring-white active:ring-primary-100 disabled:ring-neutral-100/30',
+        'disabled:cursor-not-allowed disabled:bg-neutral-300/50',
+        className,
+      )}
+    >
+      {icon}
+    </button>
+  );
 };
 
 export default IconButton;
