80023407a0ff6b522940be718dbe56bb881d2d0e
jjeunv <jjeunv@gmail.com>
2026-07-09 23:01:29 +0900

    [#8] chore: Header/SearchBar/ToggleButton export 추가

--- ipillgood-client/src/shared/components/index.ts
+++ ipillgood-client/src/shared/components/index.ts
@@ -2,9 +2,16 @@
 export { default as IconButton } from './button/IconButton';
 export { default as TextButton } from './button/TextButton';
 export { default as CheckboxButton } from './button/CheckboxButton';
+export { default as ToggleButton } from './button/ToggleButton';
 
 /* modal */
 export { default as ConfirmModal } from './modal/ConfirmModal';
 
 /* chip */
 export { default as Chip } from './Chip';
+
+/* header */
+export { Header } from './Header';
+
+/* search */
+export { SearchBar } from './SearchBar';
