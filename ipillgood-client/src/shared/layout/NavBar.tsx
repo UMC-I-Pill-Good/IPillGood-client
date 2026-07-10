b87a64beb83b6b9fae0ccd1389a19c77dcd9a520
정찬원 <jcw0522@gachon.ac.kr>
2026-07-10 21:33:55 +0900

    [#14] chore: 캐비넷 아이콘 크기 조정

--- ipillgood-client/src/shared/layout/NavBar.tsx
+++ ipillgood-client/src/shared/layout/NavBar.tsx
@@ -3,34 +3,34 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { HeartPulse, House, Search, User } from 'lucide-react';
-import { cn } from '@/shared/utils/cn';
 import { CabinetIcon } from '@/assets';
+import { cn } from '@/shared/utils/cn';
 
 const menus = [
   {
     label: '검색/랭킹',
     href: '/ranking',
-    icon: <Search />,
+    icon: Search,
   },
   {
     label: '컨디션',
     href: '/condition',
-    icon: <HeartPulse />,
+    icon: HeartPulse,
   },
   {
     label: '홈',
     href: '/',
-    icon: <House />,
+    icon: House,
   },
   {
     label: '캐비닛',
     href: '/cabinet',
-    icon: <CabinetIcon />,
+    icon: CabinetIcon,
   },
   {
     label: '마이',
     href: '/my',
-    icon: <User />,
+    icon: User,
   },
 ];
 
@@ -41,18 +41,18 @@ export const NavBar = () => {
     <nav className='fixed bottom-0 left-1/2 z-50 w-full max-w-110 -translate-x-1/2 bg-background shadow-[0_-4px_4px_0_rgba(126,131,135,0.1)]'>
       <ul className='flex h-18'>
         {menus.map(({ label, href, icon: Icon }) => {
-          const isActive = pathname === href;
+          const isActive = href === '/' ? pathname === '/' : pathname.startsWith(href);
 
           return (
             <li key={href} className='flex-1'>
               <Link
                 href={href}
                 className={cn(
-                  'flex h-full flex-col items-center justify-center gap-1 text-xs transition-colors font-medium leading-normal',
+                  'flex h-full flex-col items-center justify-center gap-1 text-xs font-medium leading-normal transition-colors',
                   isActive ? 'text-primary-600' : 'text-neutral-800',
                 )}
               >
-                {Icon}
+                <Icon className='size-5' />
                 <span>{label}</span>
               </Link>
             </li>
