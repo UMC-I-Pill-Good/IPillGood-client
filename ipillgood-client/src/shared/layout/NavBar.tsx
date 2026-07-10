6a56d06bedbe37b0719ed0aabcbf4f21b32eadf4
정찬원 <jcw0522@gachon.ac.kr>
2026-07-11 00:35:28 +0900

    [#14] chore: 메뉴 목록 이름 수정

--- ipillgood-client/src/shared/layout/NavBar.tsx
+++ ipillgood-client/src/shared/layout/NavBar.tsx
@@ -6,7 +6,7 @@ import { HeartPulse, House, Search, User } from 'lucide-react';
 import { CabinetIcon } from '@/assets';
 import { cn } from '@/shared/utils/cn';
 
-const menus = [
+const menuList = [
   {
     label: '검색/랭킹',
     href: '/ranking',
@@ -40,7 +40,7 @@ export const NavBar = () => {
   return (
     <nav className='fixed bottom-0 left-1/2 z-50 w-full max-w-110 -translate-x-1/2 shadow-[0_-4px_4px_0_rgba(126,131,135,0.1)] bg-linear-to-b from-white/40 to-white/0 backdrop-blur-xl'>
       <ul className='flex h-18 items-center justify-between'>
-        {menus.map(({ label, href, icon: Icon }) => {
+        {menuList.map(({ label, href, icon: Icon }) => {
           const isActive =
             href === '/' ? pathname === '/' : pathname === href || pathname.startsWith(`${href}/`);
 
