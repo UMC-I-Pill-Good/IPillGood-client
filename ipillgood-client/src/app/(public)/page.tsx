ea8da1a4c88cc7ba29f13f975be722588942ff1c
CHANWON <jcw0522@gachon.ac.kr>
2026-07-13 17:28:28 +0900

    [#22] feat: 온보딩 UI 구현

--- ipillgood-client/src/app/(public)/page.tsx
+++ ipillgood-client/src/app/(public)/page.tsx
@@ -1,8 +1,46 @@
+import {
+  LogoIcon,
+  Rectangle1Icon,
+  Rectangle2Icon,
+  Rectangle3Icon,
+  Rectangle4Icon,
+  Rectangle5Icon,
+} from '@/assets';
+import { Tilt_Warp } from 'next/font/google';
+
+const tiltWarp = Tilt_Warp({
+  subsets: ['latin'],
+  weight: '400',
+});
+
 const HomePage = () => {
   return (
-    <div className='flex items-center justify-center min-h-dvh text-secondary-500 text-body-5'>
-      Hello 안녕하세요
-    </div>
+    <main className='relative flex h-screen items-center justify-center overflow-hidden bg-primary-500'>
+      {/* Background */}
+      <Rectangle1Icon className='absolute -top-6 left-10' />
+
+      <Rectangle2Icon className='absolute top-30 right-24' />
+
+      <Rectangle3Icon className='absolute bottom-36 left-6' />
+
+      <Rectangle4Icon className='absolute bottom-6 right-10 z-10' />
+
+      <Rectangle5Icon className='absolute -bottom-4 right-10' />
+
+      {/* Center */}
+      <section className='relative flex flex-col items-center justify-center'>
+        <LogoIcon />
+
+        <h1
+          className={`${tiltWarp.className} absolute text-6xl font-bold text-white whitespace-nowrap [text-shadow:4px_4px_4px_rgba(0,0,0,0.15),8px_2px_40px_rgba(52,116,255,0.6)]`}
+        >
+          I Pill Good
+        </h1>
+        <p className='absolute bottom-19 text-white typo-body-10 text-center'>
+          건강한 습관의 시작, 아필굿
+        </p>
+      </section>
+    </main>
   );
 };
 
--- ipillgood-client/src/assets/index.ts
+++ ipillgood-client/src/assets/index.ts
@@ -1,2 +1,10 @@
 export { default as BadgeIcon } from './icons/badge.svg';
 export { default as CabinetIcon } from './icons/cabinet.svg';
+
+/* signup & login */
+export { default as LogoIcon } from './icons/signin/logo.svg';
+export { default as Rectangle1Icon } from './icons/signin/rectangle1.svg';
+export { default as Rectangle2Icon } from './icons/signin/rectangle2.svg';
+export { default as Rectangle3Icon } from './icons/signin/rectangle3.svg';
+export { default as Rectangle4Icon } from './icons/signin/rectangle4.svg';
+export { default as Rectangle5Icon } from './icons/signin/rectangle5.svg';
