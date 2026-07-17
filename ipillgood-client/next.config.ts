0e102ae34a656a694301046264578570c88723e6
begopa <gpdbs090@gmail.com>
2026-07-17 21:35:03 +0900

    [#21] chore : 중복된 webpack svg 로더 설정 제거

--- ipillgood-client/next.config.ts
+++ ipillgood-client/next.config.ts
@@ -11,35 +11,6 @@ const nextConfig: NextConfig = {
       },
     },
   },
-  webpack(config) {
-    const rules = config.module?.rules;
-    if (!rules) return config;
-
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const fileLoaderRule = rules.find((rule: any) =>
-      rule.test instanceof RegExp && rule.test.test('.svg')
-    );
-
-    if (fileLoaderRule) {
-      config.module.rules.push(
-        {
-          ...fileLoaderRule,
-          test: /\.svg$/i,
-          resourceQuery: /url/,
-        },
-        {
-          test: /\.svg$/i,
-          issuer: fileLoaderRule.issuer,
-          resourceQuery: { not: [/url/] },
-          use: ['@svgr/webpack'],
-        }
-      );
-
-      fileLoaderRule.exclude = /\.svg$/i;
-    }
-
-    return config;
-  },
 };
 
 export default nextConfig;
