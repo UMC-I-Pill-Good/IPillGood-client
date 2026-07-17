51823e4d3ce6a4609d58f245d0224c7ea39e5825
jjeunv <jjeunv@gmail.com>
2026-07-17 21:00:28 +0900

    [#19] fix: CheckboxList 키보드 접근성 및 readOnly 클릭 우회 버그 수정
    - 체크박스 토글을 CheckboxButton(네이티브 button)에 위임해 Tab/Enter/Space로 조작 가능하도록 수정
    - readOnly일 때 행 클릭과 CheckboxButton 클릭 모두 onToggle이 호출되지 않도록 수정
    - CheckboxButton 클릭 시 바깥 div로 이벤트가 버블링되어 토글이 중복 호출되는 문제 수정

--- ipillgood-client/src/features/home/components/CheckboxList.tsx
+++ ipillgood-client/src/features/home/components/CheckboxList.tsx
@@ -11,20 +11,28 @@ interface CheckboxListProps {
 const CheckboxList = ({ list, checkedIdList, onToggle, readOnly = false }: CheckboxListProps) => {
   return (
     <div className='flex flex-col gap-2 max-h-40 overflow-y-auto mask-t-from-99% mask-b-from-99% hide-scrollbar pb-1 pt-1.25'>
-      {list.map((item) => (
-        <div
-          key={item.id}
-          onClick={() => onToggle(item.id)}
-          className={`relative rounded-lg border border-point-700 py-3 px-5 flex justify-center ${
-            readOnly ? '' : 'cursor-pointer'
-          }`}
-        >
-          <div className='absolute left-5'>
-            <CheckboxButton checked={checkedIdList.includes(item.id)} />
+      {list.map((item) => {
+        const checked = checkedIdList.includes(item.id);
+        return (
+          <div
+            key={item.id}
+            onClick={() => {
+              if (!readOnly) onToggle(item.id);
+            }}
+            className={`relative rounded-lg border border-point-700 py-3 px-5 flex justify-center ${
+              readOnly ? '' : 'cursor-pointer'
+            }`}
+          >
+            <div className='absolute left-5' onClick={(e) => e.stopPropagation()}>
+              <CheckboxButton
+                checked={checked}
+                onClick={readOnly ? undefined : () => onToggle(item.id)}
+              />
+            </div>
+            <div className='text-point-700'>{item.label}</div>
           </div>
-          <div className='text-point-700'>{item.label}</div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 };
