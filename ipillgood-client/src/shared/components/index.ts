273df91dc146175e8058091780af20b4cd4227a1
begopa <gpdbs090@gmail.com>
2026-07-19 01:45:51 +0900

    [#27] fix : example오류 수정

--- ipillgood-client/src/shared/components/index.ts
+++ ipillgood-client/src/shared/components/index.ts
@@ -1,13 +1,24 @@
-export { default as Chip } from './Chip';
-export { default as Input } from './Input';
-export { default as ItemCard } from './ItemCard';
-export { default as SelectionCard } from './SelectionCard';
-export { SearchBar } from './SearchBar';
-
-export { default as CheckboxButton } from './button/CheckboxButton';
+/* button */
 export { default as IconButton } from './button/IconButton';
 export { default as TextButton } from './button/TextButton';
+export { default as CheckboxButton } from './button/CheckboxButton';
 export { default as ToggleButton } from './button/ToggleButton';
 
-export { default as BottomSheet } from './modal/BottomSheet';
+/* modal */
 export { default as ConfirmModal } from './modal/ConfirmModal';
+export { default as BottomSheet } from './modal/BottomSheet';
+
+/* chip */
+export { default as Chip } from './Chip';
+
+/* input */
+export { default as Input } from './Input';
+
+/* itemCard */
+export { default as ItemCard } from './ItemCard';
+
+/* search */
+export { SearchBar } from './SearchBar';
+
+/* selectionCard */
+export { default as SelectionCard } from './SelectionCard';
