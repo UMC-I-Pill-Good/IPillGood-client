d917e5a9cb4f593dd4d65f38f0cac9e5a20665f4
jjeunv <jjeunv@gmail.com>
2026-08-02 00:11:35 +0900

    [#68] feat: 영양 성분 상세 API 연동

---
+++ ipillgood-client/src/features/ingredient/hooks/useIngredientDetail.ts
@@ -0,0 +1,10 @@
+import { useQuery } from '@tanstack/react-query';
+import { getIngredientDetail } from '../api/ingredient';
+
+export const useIngredientDetail = (ingredientId: number) => {
+  return useQuery({
+    queryKey: ['ingredientDetail', ingredientId],
+    queryFn: () => getIngredientDetail(ingredientId),
+    select: (res) => res.result,
+  });
+};
