36e0d75e9a967539495a7403e150e09d75d7d4ef
jjeunv <jjeunv@gmail.com>
2026-08-03 21:17:40 +0900

    [#70] feat : 섭취 중인 영양제 목록 API 연동

--- ipillgood-client/src/features/home/components/intake-supplement-section/IntakeSupplementSection.tsx
+++ ipillgood-client/src/features/home/components/intake-supplement-section/IntakeSupplementSection.tsx
@@ -13,10 +13,10 @@ const IntakeSupplementSection = () => {
   return (
     <section className='w-full flex flex-col gap-1 mt-4'>
       <article className='flex items-center justify-between'>
-        <h2 className='typo-body-5 text-[#111111]'>섭취 중인 영양제</h2>
-        {/* TODO: 마이페이지 알림 설정으로 이동 */}
+        <h2 className='typo-body-5 text-black'>섭취 중인 영양제</h2>
+        {/* 알림 설정 */}
         <Link
-          href='/home'
+          href='/my/settings/notifications'
           className='relative flex items-center text-neutral-800 typo-caption-6 gap-0.5 h-8 px-3 rounded-full glass bg-white/10'
         >
           <Bell size={15} className='relative' />
@@ -26,8 +26,9 @@ const IntakeSupplementSection = () => {
       <section className='flex gap-3 px-8 pt-5 pb-4 rounded-[20px] border border-white bg-white/50 items-center shadow-[0px_4px_4px_0px_rgba(126,131,135,0.10)]'>
         {/* 섭취 중인 영양제 목록 */}
         <SupplementBottleList list={list} onDeleteClick={setDeleteTargetId} />
-        {/* TODO: 캐비닛 페이지로 이동 */}
-        <Link href='/home' className='transition hover:brightness-90'>
+
+        {/* 캐비닛 페이지로 이동 */}
+        <Link href='/cabinet/supplement-add' className='transition hover:brightness-90'>
           <div className='relative'>
             <SupplementBottleIcon width={43} height={70} />
             <div className='absolute left-1/2 -translate-x-1/2 top-7 flex flex-col items-center justify-center text-neutral-800 gap-1'>
