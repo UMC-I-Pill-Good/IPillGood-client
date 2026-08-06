da2f72a9308c38223f42b548d09a408d03257afe
jjeunv <jjeunv@gmail.com>
2026-08-06 23:40:24 +0900

    [#88] feat : 영양제 상세 바텀시트 연동 및 키보드 접근성 추가

--- ipillgood-client/src/features/home/components/intake-supplement-section/SupplementBottleList.tsx
+++ ipillgood-client/src/features/home/components/intake-supplement-section/SupplementBottleList.tsx
@@ -3,40 +3,83 @@
 import useEmblaCarousel from 'embla-carousel-react';
 import SupplementBottleItem from './SupplementBottleItem';
 import { ActiveProductType } from '../../types/intakeSupplement.type';
+import { useState } from 'react';
+import { ConfirmModal, SupplementDetailBottomSheet } from '@/shared/components';
+import { useIntakeSupplement } from '../../hooks/useIntakeSupplement';
 
-interface SupplementBottleListProps {
-  list: ActiveProductType[];
-  onDeleteClick: (userSupplementId: number) => void;
-}
+const SupplementBottleList = () => {
+  const [isDetailModalOpen, setIsDetailModalOpen] = useState(false);
+  const [selectedProductId, setSelectedProductId] = useState<number | null>(null);
 
-const SupplementBottleList = ({ list, onDeleteClick }: SupplementBottleListProps) => {
-  const [emblaRef] = useEmblaCarousel({
+  const { list, deleteTargetId, setDeleteTargetId, handleDeleteConfirm } = useIntakeSupplement();
+
+  const [emblaRef, emblaApi] = useEmblaCarousel({
     align: 'start',
     dragFree: true,
     containScroll: 'trimSnaps',
   });
 
+  // 키보드 접근성
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === 'ArrowLeft') emblaApi?.scrollPrev();
+    if (e.key === 'ArrowRight') emblaApi?.scrollNext();
+  };
+
   return (
-    <div
-      className='px-1 overflow-hidden mask-l-from-99% mask-r-from-95% flex-1'
-      ref={emblaRef}
-      role='region'
-      aria-roledescription='carousel'
-      aria-label='섭취 중인 영양제 목록'
-    >
-      <div className='flex gap-3'>
-        {list.map((supplement: ActiveProductType) => (
-          // TODO: 개별 영양제 클릭 모달
-          <div key={supplement.activeProductId} className='shrink-0'>
-            <SupplementBottleItem
-              productName={supplement.productName}
-              imageUrl={supplement.thumbnailImageUrl}
-              onDeleteClick={() => onDeleteClick(supplement.activeProductId)}
-            />
-          </div>
-        ))}
+    <>
+      <div
+        className='overflow-x-hidden pt-[11.5px] px-1 mask-l-from-99% mask-r-from-99%'
+        ref={emblaRef}
+        role='region'
+        aria-roledescription='carousel'
+        aria-label='섭취 중인 영양제 목록'
+        onKeyDown={handleKeyDown}
+      >
+        <div className='flex gap-3'>
+          {list.map((supplement: ActiveProductType) => (
+            <button
+              key={supplement.activeProductId}
+              className='shrink-0'
+              onClick={() => {
+                setSelectedProductId(supplement.memberProductId);
+                setIsDetailModalOpen(true);
+              }}
+            >
+              <SupplementBottleItem
+                productName={supplement.productName}
+                imageUrl={supplement.thumbnailImageUrl}
+                onDeleteClick={() => {
+                  setDeleteTargetId(supplement.activeProductId);
+                }}
+              />
+            </button>
+          ))}
+        </div>
       </div>
-    </div>
+
+      {/* 개별 영양제 클릭 모달 */}
+      <SupplementDetailBottomSheet
+        open={isDetailModalOpen}
+        onOpenChange={setIsDetailModalOpen}
+        memberProductId={selectedProductId}
+      />
+
+      {/* 개별 영양제 삭제 모달 */}
+      {deleteTargetId && (
+        <ConfirmModal
+          title={
+            <>
+              해당 영양제를 섭취 중인
+              <br />
+              영양제에서 <span className='text-semantic-600'>삭제</span>
+              하시겠습니까?
+            </>
+          }
+          onConfirm={handleDeleteConfirm}
+          onCancel={() => setDeleteTargetId(null)}
+        />
+      )}
+    </>
   );
 };
 
