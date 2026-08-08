328e522457f280526bf12850065b97111f1a74e8
정찬원 <jcw0522@gachon.ac.kr>
2026-08-09 03:45:24 +0900

    [#85] feat: 약관 페이지와 회원가입 페이지 동의 체크 상태값 공유

--- ipillgood-client/src/features/policy/components/PolicyDocumentSection.tsx
+++ ipillgood-client/src/features/policy/components/PolicyDocumentSection.tsx
@@ -1,8 +1,11 @@
 'use client';
 
 import { CheckboxButton, TextButton } from '@/shared/components';
+import { useRouter } from 'next/navigation';
 import { usePolicyDocument } from '../hooks/usePolicyDocument';
 import { DocumentCategoryType } from '../types/policy.type';
+import { useAgreementStore } from '@/features/signup/stores/useAgreementStore';
+import { agreementKeyByDocumentType } from '../constants/policy.constants';
 
 interface PolicyDocumentSectionProps {
   documentType: DocumentCategoryType;
@@ -10,9 +13,19 @@ interface PolicyDocumentSectionProps {
 }
 
 const PolicyDocumentSection = ({ documentType, isSignup }: PolicyDocumentSectionProps) => {
+  const router = useRouter();
+
   const { data, isLoading, isError } = usePolicyDocument(documentType);
   const { content, effectiveAt } = data ?? {};
+
   const formattedDate = effectiveAt?.slice(0, 10).replaceAll('-', '.');
+
+  // 동의 상태 공유
+  const checked = useAgreementStore((state) => state.checked);
+  const toggle = useAgreementStore((state) => state.toggle);
+
+  const agreementKey = agreementKeyByDocumentType[documentType];
+  const isChecked = checked[agreementKey];
 
   // 임시 (UI 미정)
   if (isError || (!isLoading && !data)) {
@@ -36,21 +49,29 @@ const PolicyDocumentSection = ({ documentType, isSignup }: PolicyDocumentSection
       )}
 
       {isSignup && <p className='typo-body-5 mt-8 mb-2'>{data?.title}</p>}
+
       {isLoading ? (
         <div className='min-h-0 flex-1 animate-pulse rounded-[20px] bg-neutral-100' />
       ) : (
-        <article className='min-h-0 flex-1 overflow-y-auto thin-scrollbar rounded-[20px] bg-white px-3 py-4 whitespace-pre-wrap leading-5 text-black mb-8'>
+        <article className='min-h-0 flex-1 overflow-y-auto thin-scrollbar rounded-[20px] bg-white px-3 py-4 whitespace-pre-wrap leading-5 text-black mb-4'>
           {content}
         </article>
       )}
+
       {isSignup && (
         <section className='shrink-0 space-y-4'>
           <div className='bg-white/50 h-11 p-4 rounded-xl glass w-full flex items-center justify-start gap-2'>
-            <CheckboxButton size='lg' />
+            <CheckboxButton checked={isChecked} onClick={() => toggle(agreementKey)} size='lg' />
             <p className='typo-body-9'>위 내용에 동의합니다</p>
           </div>
 
-          <TextButton text='확인' size='xl' className='w-full' />
+          <TextButton
+            text='확인'
+            size='xl'
+            className='w-full'
+            disabled={!isChecked}
+            onClick={() => router.back()}
+          />
         </section>
       )}
     </section>
