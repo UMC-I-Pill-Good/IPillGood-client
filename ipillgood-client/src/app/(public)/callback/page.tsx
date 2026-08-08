6ac0449c8440458b9d561762a80d9daff1c4a66d
정찬원 <jcw0522@gachon.ac.kr>
2026-08-09 04:01:45 +0900

    [#85] chore: 탈퇴 화면 체크박스 위치 조정

--- ipillgood-client/src/app/(public)/callback/page.tsx
+++ ipillgood-client/src/app/(public)/callback/page.tsx
@@ -62,7 +62,7 @@ const CallbackPage = () => {
 
   return (
     <main className='flex flex-col items-center justify-center min-h-screen gap-5'>
-      <div className='w-24 h-24 border-6 border-primary border-t-transparent rounded-full animate-spin'></div>
+      <div className='w-24 h-24 border-6 border-primary border-t-transparent rounded-full animate-spin' />
 
       <p className='text-[#6D7280] font-medium text-lg'>로그인 처리 중...</p>
     </main>
