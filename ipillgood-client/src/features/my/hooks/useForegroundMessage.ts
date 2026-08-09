97f67a7749ad2a5818596de01adbbbdc5e0ada70
jjeunv <jjeunv@gmail.com>
2026-08-09 16:45:31 +0900

    [#95] feat : 포그라운드 푸시 메세지 수신 리스너 추가

--- ipillgood-client/src/features/my/hooks/useForegroundMessage.ts
+++ ipillgood-client/src/features/my/hooks/useForegroundMessage.ts
@@ -3,7 +3,7 @@ import { onMessage } from 'firebase/messaging';
 import { getMessagingInstance } from '@/shared/utils/firebase';
 
 // 탭이 포그라운드(활성 상태)일 때는 서비스워커가 알림을 자동으로 띄워주지 않으므로
-// 클라이언트에서 직접 메시지를 받아 알림으로 표시함
+// 클라이언트에서 직접 메시지를 받아 알림으로 표시
 export const useForegroundMessage = () => {
   useEffect(() => {
     let unsubscribe: (() => void) | undefined;
