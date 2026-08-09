98f382c2d9cd956f08ae03fa5d77b760f90ab134
jjeunv <jjeunv@gmail.com>
2026-08-10 05:43:27 +0700

    [#125] fix : 마이페이지 노션 링크 오류 및 설문 재작성 버튼 미연결 수정

--- ipillgood-client/src/features/my/components/about-section/AboutSection.tsx
+++ ipillgood-client/src/features/my/components/about-section/AboutSection.tsx
@@ -5,7 +5,7 @@ import { TextButton } from '@/shared/components';
 
 const AboutSection = () => {
   return (
-    <section className='flex flex-col flex-1 items-center px-5 pt-22 pb-22'>
+    <section className='flex flex-col flex-1 items-center px-5 pt-22 pb-28.5'>
       <MascotBigIcon />
       <p className='mt-15 mb-1 typo-body-6 text-black'>아필굿은 개인 맞춤 영양 성분 추천을 통해</p>
       <p className='typo-body-6 text-black mb-26'>
@@ -13,17 +13,28 @@ const AboutSection = () => {
       </p>
 
       <div className='flex flex-col gap-2 w-full mt-auto'>
-        {/* TODO: 노션 url로 이동 */}
         <TextButton
           type='button'
-          onClick={() => window.open('/', '_blank', 'noopener,noreferrer')}
+          onClick={() =>
+            window.open(
+              'https://grandiose-front-c83.notion.site/3b72a4a6a3d0800f8a06f20ff0a3d838?source=copy_link',
+              '_blank',
+              'noopener,noreferrer',
+            )
+          }
           text='서비스 소개 (Notion)'
           size='xl'
           className='w-full'
         />
         <TextButton
           type='button'
-          onClick={() => window.open('/', '_blank', 'noopener,noreferrer')}
+          onClick={() =>
+            window.open(
+              'https://grandiose-front-c83.notion.site/3b72a4a6a3d080aa820fccccacd70c5f?source=copy_link',
+              '_blank',
+              'noopener,noreferrer',
+            )
+          }
           text='오픈소스 기술 (Notion)'
           size='xl'
           className='w-full'
