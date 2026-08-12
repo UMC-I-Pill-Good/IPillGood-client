f915de79019225023499fa4a32f992cf88feb7f2
정찬원 <jcw0522@gachon.ac.kr>
2026-08-12 20:18:07 +0900

    [#161] chore: siteUrl 슬래시를 정규화

--- ipillgood-client/src/app/robots.ts
+++ ipillgood-client/src/app/robots.ts
@@ -1,6 +1,9 @@
 import type { MetadataRoute } from 'next';
 
-const siteUrl = process.env.NEXT_PUBLIC_SITE_URL ?? 'https://www.ipillgood.store';
+const siteUrl = (process.env.NEXT_PUBLIC_SITE_URL ?? 'https://www.ipillgood.store').replace(
+  /\/+$/,
+  '',
+);
 
 export default function robots(): MetadataRoute.Robots {
   return {
